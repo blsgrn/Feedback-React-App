f03f42c505ee1aa0d7dd933cd1ce32b9db15c1c7
Sanatana Sekram <blsgrn@tutanota.com>
2022-03-29 23:52:47 +0530

    Add delete button to items throug prop drill

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,13 +1,25 @@
-// import React from 'react'
+import { FaTimes } from 'react-icons/fa';
 import Card from './shared/Card';
+import PropTypes from 'prop-types';
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+  // const handleClick = (id) => {
+  // console.log(id);
+  // };
+
   return (
-    <Card reverse={true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes onClick={() => handleDelete(item.id)} color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
