d74a246a0ed0be80233415a1fb5bb7c87ea49d12
Sanatana Sekram <blsgrn@tutanota.com>
2022-03-31 01:13:12 +0530

    Create About Page using react-router-dom

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -14,21 +16,36 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   };
 
-  const deleteFeedbaack = (id) => {
+  const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList handleDelete={deleteFeedbaack} feedback={feedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  handleDelete={deleteFeedback}
+                  feedback={feedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+          {/* <Route path="/about" component={AboutPage} /> */}
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+          temporibus, consequatur nihil laudantium dolore libero praesentium
+          nemo optio quisquam quae!
+        </p>
+        <p>
+          <a href="/">back to home</a>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
