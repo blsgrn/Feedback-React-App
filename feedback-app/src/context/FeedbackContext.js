bc75670a47a89c797e85584eac2351fc102f38ef
Sanatana Sekram <blsgrn@tutanota.com>
2022-04-02 22:47:27 +0530

    install npm server

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,19 +1,21 @@
-import { v4 as uuidv4 } from 'uuid';
-import { createContext, useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: 'This is item 01', rating: 10 },
-    { id: 2, text: 'This is item 02', rating: 9 },
-    { id: 3, text: 'This is item 03', rating: 7 },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    console.log(1, 2, 3);
+  }, []);
+
+  
 
   // add feedback
 
@@ -25,7 +27,7 @@ export const FeedbackProvider = ({ children }) => {
   // delete feedback
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
